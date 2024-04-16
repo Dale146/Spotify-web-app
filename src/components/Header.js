de22cdb649a4962e20cfac9e7279562a1cfded8b
Dale146 <150839726+Dale146@users.noreply.github.com>
2024-04-16 16:16:56 -0700

    stage 1

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import spotifyLogo from "../images/spotify.svg";
 function Header() {
     const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
     const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
-    const REDIRECT_URI = "https://hzhengweb.com/my-app/pages/Home";
+    const REDIRECT_URI = "http://localhost:3000/my-app/pages/Home";
     const RESPONSE_TYPE = "token"
 
     const AUTH_ENDPOINT = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-email%20user-read-private%20user-follow-read%20user-top-read`;
